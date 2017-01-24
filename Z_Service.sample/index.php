57f9c220e108863351fa9355c0846429f8f0dd3f
bartonjoe <bartonjoe@bartonjoe-VirtualBox>
2017-01-24 18:50:29 +0800

    ZFrame Service sample normal logic

--- Z_Service.sample/index.php
+++ Z_Service.sample/index.php
@@ -1,3 +1,33 @@
 <?php
-// put your code here
+
+// Service sample logical
+// Initlize    20170124    Joe
+
+require_once 'Config/SqlDef.php';
+require_once 'ZConnect/PDO.php';
+
+$ZData = json_decode(file_get_contents("php://input"));
+if (empty($ZData)) {
+    echo 'No post data!';
+    return;
+}
+//var_dump($ZData->head);
+//var_dump($ZData);
+//echo constant('sample.select.getData');
+
+$con = new \ZFrame_Service\ZConnect();
+$pdo = $con->_getPdo();
+
+$userById = $con->getUserById($ZData->body);
+$inner = $con->getInnerJoin();
+
+$result = new \stdClass();
+$result->user=$userById;
+$result->syohinInner=$inner;
+
+//echo '<br/>##################ServiceLine#################<br/>';
+echo json_encode($result);
+//echo '<br/>##################ServiceLine#################<br/>';
+
+$con = NULL;
 ?>
