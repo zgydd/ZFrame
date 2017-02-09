f0194ba1d1b43d9dc17067bdc9690efae11955f3
bartonjoe <bartonjoe@bartonjoe-VirtualBox>
2017-02-09 16:52:30 +0800

    Include Libra from Lucy Start version --V0.0.10

---
+++ ZService.multiSample001/Config/SqlDef.php
@@ -0,0 +1,23 @@
+<?php
+
+// Service sample SQL
+// Initlize    20170124    Joe
+
+define('sample.select.getData', <<<SQL
+    SELECT `name`, `value` FROM M_USER WHERE id = :id
+SQL
+);
+
+define('sample.select.getInner', <<<SQL
+    SELECT
+        M_USER.name AS uName,
+        M_USER.value AS uValue,
+        M_PRODUCT.name as pName,
+        M_PRODUCT.value AS pValue
+    FROM
+        M_PRODUCT
+    INNER JOIN
+        M_USER ON (M_PRODUCT.user_id = M_USER.id)
+    WHERE M_USER.id = :id
+SQL
+);
---
+++ ZService.multiSample001/index.php
@@ -0,0 +1,53 @@
+<?php
+
+// Service sample logical
+// Initlize    20170124    Joe
+
+require_once 'Config/SqlDef.php';
+require_once 'ZConnect/PDO.php';
+
+//Enter
+$ZData = file_get_contents("php://input");
+if ($ZData == 'Z_TEST_TIMESTAMP') {
+    //sleep(1);
+    echo microtime(true);
+    return;
+}
+$ZData = json_decode($ZData);
+if (is_null($ZData) || empty($ZData)) {
+    echo 'Z_MSG_NO_POSTDATA';
+    return;
+}
+
+echo '<br/>##################ServiceLine#################<br/>';
+echo '--Start init connection at ' . microtime(TRUE) . '<br/>';
+$con = new \ZFrame_Service\ZConnect();
+echo '--Start get pdo at ' . microtime(TRUE) . '<br/>';
+$pdo = $con->_getPdo();
+echo '--Select user at ' . microtime(TRUE) . '<br/>';
+$userById = $con->getUserById($ZData->entity->body);
+echo '--Get data at ' . microtime(TRUE) . '<br/>';
+$inner = $con->getInnerJoin($ZData->entity->body);
+
+echo '--Create result at ' . microtime(TRUE) . '<br/>';
+$result = new \stdClass();
+$result->user=$userById;
+$result->syohinInner=$inner;
+
+$ZData->head->dataTo = $ZData->head->dataFrom;
+$ZData->routeLine = $ZData->head->dataFrom;
+$ZData->head->dataFrom = NULL;
+$ZData->entity = $result;
+
+//Route Check
+echo json_encode($ZData);
+echo '<br/>';
+echo '--Feed back at ' . microtime(TRUE) . '<br/>';
+echo '<br/>##################ServiceLine#################<br/>';
+
+////Data Check
+//echo json_encode($ZData);
+////echo '<br/>';
+////print_r($ZData);
+
+$con = NULL;
